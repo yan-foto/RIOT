bc75d9497538d8f0db15d463107660a7cb7ea6e8
Yan Foto <yan.foto@quaintous.com>
2020-08-24 21:48:25 +0200

    First attempt to integrate NDN Lite as RIOT package.
    Currently to use NDN Lite with RIOT you need an outdated fork of
    RIOT, a modified (and outdated) branch of NDN Lite (ndn-riot), and
    a set of glue code (ndn-lite-over-riot). This complicated setup,
    however, can massively be simplified using RIOT's packaging
    mechanism as explained below.
    
    This preliminary version of NDN Lite package is conceptualized as
    follows:
    1. As NDN Lite does not follow a specific release management
    or versioning strategy, the latest version (as of Aug 2020) was
    simply used with minor modifications (see patches).
    2. The glue code needed to port NDN Lite to RIOT was directly
    copied from the `ndn-lite-over-riot` project (adaptation folder)
    with only minor changes (e.g., to avoid compiler errors). The
    directory structure was however updated.
    
    NOTE: the code has not been tested properly at this stage. It just
    "builds"!

---
+++ pkg/ndn-lite/contrib/ndn-lite.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2019 Tianyuan Yu
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ *
+ * See AUTHORS.md for complete list of NDN IOT PKG authors and contributors.
+ */
+
+#include <net/gnrc/netapi.h>
+#include <net/gnrc/netif.h>
+#include <net/gnrc/netreg.h>
+
+#include "ndn-lite/forwarder/forwarder.h"
+#include "ndn-lite/security/ndn-lite-sec-config.h"
+
+#define GNRC_NDN_LITE_MSG_QUEUE_SIZE    (8U)
+
+// typedef struct ndn_lite_instance {
+//   // reference to forwarder
+//   ndn_forwarder_t* forwarder;
+
+//   // RIOT message queue
+//   msg_t msg;
+//   msg_t reply;
+//   msg_t msg_q[GNRC_NDN_LITE_MSG_QUEUE_SIZE];
+
+//   // RIOT network stack registeration entry
+//   gnrc_netreg_entry_t me_reg;
+// } ndn_lite_instance_t;
+
+void
+ndn_lite_startup(void);
