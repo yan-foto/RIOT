fa3d70d9c111fa8e68a3a5899515bde0ec217155
Yan Foto <yan.foto@quaintous.com>
2020-08-24 21:48:25 +0200

    First attempt to integrate NDN Lite as RIOT package.
    Currently to use NDN Lite with RIOT you need an outdated fork of
    RIOT, a modified (and outdated) branch of NDN Lite (ndn-riot), and
    a set of glue code (ndn-lite-over-riot). This complicated setup,
    however, can massively be simplified using RIOT's packaging
    mechanism as explained below.
    
    This preliminary version of NDN Lite package is conceptualized as
    follows:
    1. As NDN Lite does not follow a specific release management
    or versioning strategy, the latest version (as of Aug 2020) was
    simply used with minor modifications (see patches).
    2. The glue code needed to port NDN Lite to RIOT was directly
    copied from the `ndn-lite-over-riot` project (adaptation folder)
    with only minor changes (e.g., to avoid compiler errors). The
    directory structure was however updated.
    
    NOTE: the code has not been tested properly at this stage. It just
    "builds"!

---
+++ pkg/ndn-lite/contrib/ndn-lite.c
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2019 Tianyuan Yu
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ *
+ * See AUTHORS.md for complete list of NDN IOT PKG authors and contributors.
+ */
+
+#include "ndn-lite.h"
+#include "netface/netface.h"
+#include "security/rng-riot-impl.h"
+#include "util/time.h"
+#include "ndn-lite/forwarder/forwarder.h"
+
+void
+ndn_lite_startup()
+{
+  register_platform_security_init(ndn_lite_riot_rng_load_backend);
+  ndn_security_init();
+  ndn_forwarder_init();
+  ndn_netface_auto_construct();
+  // ndn_netface_traverse_print();
+}
---
+++ pkg/ndn-lite/contrib/netface/netface.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2019 Tianyuan Yu
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ *
+ * See AUTHORS.md for complete list of NDN IOT PKG authors and contributors.
+ */
+
+#ifndef NDN_NETFACE_H_
+#define NDN_NETFACE_H_
+
+#include "ndn-lite/forwarder/face.h"
+#include "ndn-lite/encode/fragmentation-support.h"
+#include <kernel_types.h>
+#include <thread.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * Link layer face.
+ */
+typedef struct ndn_netface {
+  /*
+   * The inherited interface.
+   */
+  ndn_face_intf_t intf;
+  /*
+   * Link layer MTU.
+   */
+  uint16_t mtu;
+  /*
+   * Re-assembly buffer.
+   */
+  uint8_t frag_buffer[500];
+  /*
+   * Assembler help the re-assembly.
+   */
+  ndn_frag_assembler_t assembler;
+  /*
+   * Corresponding link layer PID.
+   */
+  kernel_pid_t pid;
+} ndn_netface_t;
+
+/*
+ * Initializes the netif table and try to add existing
+ * network devices into the netif and face tables.
+ * @return the initialized netfaces.
+ */
+uint32_t ndn_netface_auto_construct(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* NDN_NETFACE_H_ */
+ /** @} */
